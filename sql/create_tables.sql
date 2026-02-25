4f25cdea2bb41fd1900b6e0ab49610dd527bcb46
matin0789 <matin.a0789@gmail.com>
2026-02-25 21:46:27 +0330

    feat(sql): add discount_codes table definition to create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -21,7 +21,7 @@ CREATE TABLE booths (
     status_end_date DATE
 )
 
--- SUPPORT TABLE
+-- SUPPORT TABLES
 CREATE TABLE supports (
     Employee_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     fname VARCHAR(100) NOT NULL,
@@ -30,11 +30,27 @@ CREATE TABLE supports (
     password VARCHAR(512)
 )
 
--- ACTION_LOG
+-- ACTION_LOG TABLES
 CREATE TABLE action_logs (
     log_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     ip_address INET NOT NULL,
     action_type VARCHAR(100),
     description TEXT,
     timestamp TIMESTAMP
+)
+
+-- DISCOUNT_TYPE ENUMS
+CREATE TYPE DISCOUNT_TYPE AS ENUM ('FIXED', 'PERCENTAGE');
+-- DISCOUNT_CODE TABLES
+CREATE TYPE discount_codes (
+    code_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    code VARCHAR(20) UNIQUE NOT NULL,
+    is_used BOOLEAN DEFAULT NOT NULL,
+    expiration_date DATE,
+    discount_type DISCOUNT _TYPE NOT NULL,
+    amount BIGINT,
+    percent DECIMAL(5, 2) CHECK (percent BETWEEN 0 AND 100),
+
+    CONSTRAINT check_amount_or_percent_not_null
+        CHECK (amount IS NOT NULL OR percent IS NOT NULL)
 )
