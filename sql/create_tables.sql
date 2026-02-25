ea49339e3fe93f4a7d2e2206283a0310b45329a6
matin0789 <matin.a0789@gmail.com>
2026-02-25 21:27:13 +0330

    feat(sql): add booths table definition to create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -10,3 +10,13 @@ CREATE TABLE users (
     CONSTRAINT check_email_or_phone_not_null
         CHECK (phone IS NOT NULL OR email IS NOT NULL)
 );
+
+-- BOOTH TABLES
+CREATE TABLE booths (
+    booth_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    booth_name VARCHAR(100) NOT NULL,
+    image_url VARCHAR(200),
+    description TEXT,
+    status_count INT CHECK (status_count BETWEEN 0 AND 6)
+    status_end_date DATE
+)
